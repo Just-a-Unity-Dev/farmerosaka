c5e29a58114c26d11469556eb4f1f857aff692d2
Rain Caldwell <rain@is-going-to.cyou>
2025-11-17 20:46:16 +0800

    save corpus to file

--- cogs/message_recorder.py
+++ cogs/message_recorder.py
@@ -3,6 +3,8 @@ from discord import Message
 from discord.ext import tasks
 import os
 
+from classes.is_owner import is_owner
+
 
 class RecorderCog(
     commands.Cog,
@@ -18,7 +20,7 @@ class RecorderCog(
         self.opt_out_id = int(os.getenv('MESSAGE_RECORDER_OPT_OUT_ROLE_ID'))
         self.messages = []
 
-        with open("corpus.txt", "r") as f:
+        with open("corpus.txt", "w+") as f:
             self.corpus = f.read().strip()
 
     @commands.Cog.listener()
@@ -31,11 +33,24 @@ class RecorderCog(
         self.today_messages.append([message.content, message.author.id, message.jump_url])
         self.corpus += " " + message.content
 
+    @commands.hybrid_command(
+            name="savecorpus",
+            brief="saves corpus",
+            description="saves the corpus"
+    )
+    @is_owner()
+    async def pfp_command(self, ctx: commands.Context):
+        self.save_corpus()
+        await ctx.reply("Saved corpus.")
+
+    def save_corpus(self):
+        with open("corpus.txt", "w") as f:
+            f.write(self.corpus.strip())
+        print("Saved corpus to file.")
+
     @tasks.loop(minutes=60)
     async def autosave_corpus(self):
-        with open("corpus.txt", "w") as f:
-            f.write(self.corpus)
-        print("Autosaved corpus.")
+        self.save_corpus()
 
 
 async def setup(client: commands.Bot) -> None:
